6a9888d15aff0662de6e3130b30642f608be1cc4
Charanagit <charanakandy@gmail.com>
2026-03-05 21:29:37 +0800

    patch onnxruntime execstack for Cloud

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Patching onnxruntime .so to remove execstack flag..."
-apt-get update -qq && apt-get install -y execstack -qq
-execstack -c /home/adminuser/venv/lib/python3.11/site-packages/onnxruntime/capi/onnxruntime_pybind11_state*.so
-echo "Patch complete - execstack cleared"
+echo "Patching onnxruntime for execstack issue..."
+apt update && apt install -y execstack
+execstack -c $VIRTUAL_ENV/lib/python3.11/site-packages/onnxruntime/capi/onnxruntime_pybind11_state*.so
+echo "Patch done."
